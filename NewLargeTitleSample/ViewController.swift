0d53c5aad072135437f7c3221666be2f7ca0e867
Takuya Aso <milanista224@gmail.com>
2019-10-04 19:03:21 +0900

    Implemented foundation for this sample App.

--- NewLargeTitleSample/ViewController.swift
+++ NewLargeTitleSample/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let cellName = "Cell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate { }
+
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellName)
+        cell?.textLabel?.text = "Cell " + String(indexPath.row)
+        return cell!
+    }
+}
