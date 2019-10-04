763fed6469c5bd795413d221361a061984688e7b
Takuya Aso <milanista224@me.com>
2019-10-04 18:46:53 +0900

    Initial Commit

---
+++ NewLargeTitleSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewLargeTitleSample
+//
+//  Created by Takuya Aso on 2019/10/04.
+//  Copyright © 2019 Takuya Aso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
